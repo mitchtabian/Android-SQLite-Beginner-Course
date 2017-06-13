14c3a4abe644b386570f562a041212a6738f4ce7
Mitch <mitch@tabian.ca>
2017-06-13 14:32:36 -0700

    part22- Edit Contact Fragment Widgets

--- ContactsList/app/src/main/java/codingwithmitch/com/contactslist/EditContactFragment.java
+++ ContactsList/app/src/main/java/codingwithmitch/com/contactslist/EditContactFragment.java
@@ -3,13 +3,20 @@ package codingwithmitch.com.contactslist;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Spinner;
 
+
+import codingwithmitch.com.contactslist.Utils.UniversalImageLoader;
 import codingwithmitch.com.contactslist.models.Contact;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * Created by User on 6/12/2017.
@@ -24,16 +31,48 @@ public class EditContactFragment extends Fragment{
         setArguments(new Bundle());
     }
 
+    private Contact mContact;
+    private EditText mPhoneNumber, mName, mEmail;
+    private CircleImageView mContactImage;
+    private Spinner mSelectDevice;
+    private Toolbar toolbar;
+    private String mSelectedImagePath;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_editcontact, container, false);
+        mPhoneNumber = (EditText) view.findViewById(R.id.etContactPhone);
+        mName = (EditText) view.findViewById(R.id.etContactName);
+        mEmail = (EditText) view.findViewById(R.id.etContactEmail);
+        mContactImage = (CircleImageView) view.findViewById(R.id.contactImage);
+        mSelectDevice = (Spinner) view.findViewById(R.id.selectDevice);
+        toolbar = (Toolbar) view.findViewById(R.id.editContactToolbar);
         Log.d(TAG, "onCreateView: started.");
 
-        getContactFromBundle();
+        //get the contact from the bundle
+        mContact = getContactFromBundle();
 
+        if(mContact  != null){
+            init();
+        }
 
         return view;
+    }
+
+    private void init(){
+        mPhoneNumber.setText(mContact.getPhonenumber());
+        mName.setText(mContact.getName());
+        mEmail.setText(mContact.getEmail());
+        UniversalImageLoader.setImage(mContact.getProfileImage(), mContactImage, null, "http://");
+
+        //Setting the selected device to the spinner
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),
+                R.array.device_options, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mSelectDevice.setAdapter(adapter);
+        int position = adapter.getPosition(mContact.getDevice());
+        mSelectDevice.setSelection(position);
     }
 
     /**
